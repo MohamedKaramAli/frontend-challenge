deab3625977b97e65b54e6a837717de74b605e0a
MohamedKaramAli <mohamed.karam.ali@gmail.com>
2021-05-31 21:44:13 +0200

    implement on scroll functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,33 +12,43 @@ export class AppComponent implements OnInit {
   constructor(private  _httpClient:HttpClient){}
   ngOnInit()
   {
-      this._httpClient.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
-
-      .subscribe
-      ((res:any)=>
-      {
-        ((res.items) as any[]).forEach((v,i)=>{
-
-
-          let currentDate = new Date().getTime()
-          try
-          {
-            let creationDate =  (new Date(v.created_at) ).getTime ();
-            let miliSecondsPerDay =   (1000 * 60 * 60 * 24);
-            let diffInMiliSeconds = currentDate- creationDate;
-            v.timeInterval = Math.ceil(diffInMiliSeconds /  miliSecondsPerDay) ;
-            // iam not sure if this is the correct way to calculate time interval
-
-          }catch{}
-        });
-        this.repos.items = this.repos.items.concat( res.items);
-      }
-
-      )
+this.getByPage(1);
   }
 
+  pageIndex = 1;
+  getByPage( page:number)
+  {
+
+        let reposUrl=    "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc"
+        if(page>1)
+          reposUrl += "&page="+this.pageIndex
+
+        this._httpClient.get(reposUrl)
+
+        .subscribe
+        ((res:any)=>
+        {
+          ((res.items) as any[]).forEach((v,i)=>{  
+            let currentDate = new Date().getTime()
+            try
+            {
+              let creationDate =  (new Date(v.created_at) ).getTime ();
+              let miliSecondsPerDay =   (1000 * 60 * 60 * 24);
+              let diffInMiliSeconds = currentDate- creationDate;
+              v.timeInterval = Math.ceil(diffInMiliSeconds /  miliSecondsPerDay) ;
+              // iam not sure if this is the correct way to calculate time interval
+  
+            }catch{}
+          });
+          this.repos.items = this.repos.items.concat( res.items);
+        }
+  
+        )
+  
+      }
   onScroll(){
-    alert("scrolled")
+   this.pageIndex = this.pageIndex+1;
+   this.getByPage(this.pageIndex)
   }
 }
 
