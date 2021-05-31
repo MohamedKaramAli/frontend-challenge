02e964806bb956c2b1ef28e18b4062e3b12d8fa7
MohamedKaramAli <mohamed.karam.ali@gmail.com>
2021-05-31 21:23:00 +0200

    add ngx-infinite-scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend-challenge';
+export class AppComponent implements OnInit {
+  title = 'Trending Repo';
+  repos:any = {items:[]};
+  constructor(private  _httpClient:HttpClient){}
+  ngOnInit()
+  {
+      this._httpClient.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+
+      .subscribe
+      ((res:any)=>
+      {
+        ((res.items) as any[]).forEach((v,i)=>{
+
+
+          let currentDate = new Date().getTime()
+          try
+          {
+            let creationDate =  (new Date(v.created_at) ).getTime ();
+            let miliSecondsPerDay =   (1000 * 60 * 60 * 24);
+            let diffInMiliSeconds = currentDate- creationDate;
+            v.timeInterval = Math.ceil(diffInMiliSeconds /  miliSecondsPerDay) ;
+            // iam not sure if this is the correct way to calculate time interval
+
+          }catch{}
+        });
+        this.repos.items = this.repos.items.concat( res.items);
+      }
+
+      )
+  }
+
+  onScroll(){
+    alert("scrolled")
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    InfiniteScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
